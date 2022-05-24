c2caf261b7e796f803f192a34b611ca219b15d12
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-24 16:25:13 +0200

    fixing the routes for booking controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'booking/new'
-  get 'booking/create'
-  get 'booking/destroy'
+  # get 'booking/new'
+  # get 'booking/create'
+  # get 'booking/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :gardens
-
+  resources :gardens do
+    resources :booking, only: [:new , :create, :update, :edit]
+  end
   get '/dashboard', to: 'dashboards#dashboard'
 
   # Defines the root path route ("/")

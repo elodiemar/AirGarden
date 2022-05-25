a2260809573a42de911d1c8bf276aec14b660db0
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-25 13:51:46 +0200

    remove seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require "faker"
-User.create(
-  first_name: "Elodie",
-  last_name: "Mariton",
-  email: "e.mariton@hotmail.fr",
-  password: "123456"
- )
- puts "user created"
+# require "faker"
+# User.create(
+#   first_name: "Elodie",
+#   last_name: "Mariton",
+#   email: "e.mariton@hotmail.fr",
+#   password: "123456"
+#  )
+#  puts "user created"
 
-10.times do
-    user = User.first
-    p garden = Garden.create(
-    title: Faker::Movie.title,
-    description: Faker::Movie.quote,
-    capacity: rand(0..10),
-    size: rand(0..1000),
-    price_per_hour: rand(0..20),
-    user_id: user.id
-  )
-   garden.save!
-end
+# 10.times do
+#     user = User.first
+#     p garden = Garden.create(
+#     title: Faker::Movie.title,
+#     description: Faker::Movie.quote,
+#     capacity: rand(0..10),
+#     size: rand(0..1000),
+#     price_per_hour: rand(0..20),
+#     user_id: user.id
+#   )
+#    garden.save!
+# end
 
-puts " created 10 fakes garden"
+# puts " created 10 fakes garden"

41074c22f63c4a56dd30968d69e4a1938874bbb6
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-26 16:19:48 +0200

    review controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,7 @@ class DashboardsController < ApplicationController
     @gardens = current_user.gardens
     @bookings = current_user.bookings
     authorize :dashboard, :dashboard?
+    @review = Review.new
     # il va chercher dans le dashboard policy chercher la methode dashboard
   end
 end

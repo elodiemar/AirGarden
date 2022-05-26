22a018119ad5dbd03b43830b9690b6186d3973da
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-26 18:07:39 +0200

    bookings next and past

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,8 @@ class DashboardsController < ApplicationController
   def dashboard
     @gardens = current_user.gardens
     @bookings = current_user.bookings
+    @past_bookings = Booking.where("END_DATE < ? AND USER_ID = ?", Time.now, current_user.id)
+    @next_bookings = Booking.where("END_DATE > ? AND USER_ID = ?", Time.now, current_user.id)
     authorize :dashboard, :dashboard?
     @review = Review.new
     authorize @review
